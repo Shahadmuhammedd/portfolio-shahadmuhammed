2a4db639b4faa72a0ef5e01ea8cf5aa1dc43e7eb
Shahad Muhammed <shahadmuhammed21@gmail.com>
2024-04-25 17:03:03 +0530

    portfolio-shahadmuhammed

---
+++ src/data/constants.js
@@ -0,0 +1,236 @@
+export const Bio = {
+  name: "Shahad Muhammed",
+  roles: [
+    "MEARN Stack Developer",
+    "REACT Developer",
+   
+  ],
+  description:
+    "Welcome to my portfolio! I'm a dedicated MEARN stack developer passionate about crafting robust web applications. With expertise in MongoDB, Express.js, Angular, and Node.js, I specialize in building dynamic and responsive solutions tailored to meet client needs.",
+  github: "https://github.com/Shahadmuhammedd",
+  resume:
+    "https://drive.google.com/file/d/1EquEY0_D9_WV9OIEEnaPF2YZQ4y-_hoW/view?usp=sharing",
+  linkedin: "www.linkedin.com/in/shahad-muhammed-a01a0729b",
+  
+  insta: "https://www.instagram.com/shahadmuhammed._?igsh=MWFpMTR0ZGR6M2p0MA==",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://vectorified.com/images/express-js-icon-20.png",
+      },
+     
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Angular",
+        image: "https://www.freecodecamp.org/news/content/images/2020/04/Copy-of-Copy-of-Travel-Photography.png",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://tse3.mm.bing.net/th?id=OIP.M2hdh3gMLsXxAZXyK2p14AHaHa&pid=Api&P=0&h=180",
+    role: "Web Developer",
+    company: "Luminar Technolab",
+    date: "Nov 2023 - Current",
+    desc: "Oversee and maintain core Web services and Websites.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaSript",
+      "ReactJS",
+      "NodeJs",
+      "Exress",
+      "MongoDB",
+      "Angular",
+      
+    ],
+    
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "http://mietindia.ac.in/images/bharathiar.png",
+    school: "Nilgiri College Of Arts And Science, Bharathiar University",
+    date: "2020 - 2023",
+    grade: "82%",
+    desc: "I am proud to have completed my Bachelor of Science in Computer Science from Nilgiri College of Arts and Science, an institution affiliated with NAAC. Throughout my academic journey, I successfully navigated my coursework without encountering any academic setbacks, demonstrating my commitment to excellence and dedication to my field of study. This experience has not only equipped me with a strong foundation in computer science but also instilled in me a sense of discipline and resilience that I bring to every endeavor",
+    degree: " Bachelor of Science in Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://bingo.icbse.com/business.jpg?action=logo&id=x2d69z",
+    school: "GHSS Moolankave,wayanad",
+    date: " 2018 -  2020",
+    grade: "78.2%",
+    desc: "I completed my class 12 high school education at Ghss Moolankave School, wayanad, where I studied Science with Biology.",
+    degree: "XII, Science with Biology",
+  },
+  {
+    id: 2,
+    img: "https://bingo.icbse.com/business.jpg?action=logo&id=3mjmng",
+    school: "Ghss Mathamangalam,wayanad",
+    date: "2018",
+    grade: "95.3%",
+    desc: "I completed my class 10 education at Ghss Mathamangalam,wayanad, where I studied Science.",
+    degree: "X, Science ",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Doctor's Appoinment Booking",
+    date: "March 2024 - April 2023",
+    description:
+      "Oversee and maintain core web services and website,contain a Login & Register , And only the registered person will get access to the Home Page,Collaborate closely with senior developers to enhance website functionality, introducing new features such as appointment scheduling and doctor discovery functionalities,implement features enabling users to schedule appointments and easily find relevant doctors, enhancing the overall user experience and usability of the platform",
+    image:
+      "https://github.com/Shahadmuhammedd/BookMyDoc-frontend/assets/127824263/98299de8-1855-41e8-b41a-70e334490a16",
+    tags: [
+      "React JS",
+      "Angular",
+      "Node",
+      "MongoDB",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/Shahadmuhammedd/BookMyDoc-frontend",
+    webapp: "https://www.linkedin.com/posts/shahad-muhammed-a01a0729b_reactjs-node-axios-activity-7187371040795148288-xQ_J?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    id: 0,
+    title: "Project Fair ",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "User Authentication: Implemented user authentication using Node.js and Express.js ,allowing registered users to log in securely,Project Management: Designed a feature for registered users to add, view, update, and delete project details using React.js for the frontend interface and MongoDB for database storage.Technology Stack: Utilized React.js for the frontend, Node.js and Express.js for thebackend server, and MongoDB for database management.",
+    image:
+      "https://github.com/Shahadmuhammedd/project-fair-frontend/assets/127824263/2759503e-a277-4dd3-bf5f-03a56f4bf652",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js"],
+    category: "web app",
+    github: "https://github.com/Shahadmuhammedd/project-fair-backend",
+    webapp: "",
+  
+  },
+  {
+    id: 1,
+    title: "Restaurant Listing App",
+    date: "jan 2024-jan-2024",
+    description:
+      "Designed and developed the Restaurant listing project,",
+    image:
+      "https://github.com/Shahadmuhammedd/Restaurent-listing-frontend/assets/127824263/44568ea6-ecd9-45f9-8af1-306dc8cfb6c0",
+    tags: [
+      "React Js",
+      "React Bootstrap",
+      "Json Server",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/Shahadmuhammedd/Restaurent-listing-frontend",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "Employee Management System",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "The Employee Management System (EMS) built using React and JSON data is a web-based application designed to streamline the management of employee-related tasks within an organization.",
+    image:
+      "https://github.com/Shahadmuhammedd/Ems-Frontend/assets/127824263/f475addc-3612-4dcd-b379-7bba5d653051",
+    tags: [ "React","JavaScript,Json server"],
+    category: "machine learning",
+    github: "https://github.com/Shahadmuhammedd/Ems-Frontend",
+    webapp: "",
+   
+  },
+ 
+  
+  
+];
+
